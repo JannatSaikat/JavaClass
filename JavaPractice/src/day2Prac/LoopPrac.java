82fc005b0c7e9c7d38745699a749575f3b9f30ad
Jannat Saikat <jannatsaikat@gmail.com>
2020-05-27 20:32:45 -0400

    day2 for loop, while loop, switch case

---
+++ JavaPractice/src/day2Prac/LoopPrac.java
@@ -0,0 +1,25 @@
+package day2Prac;
+
+public class LoopPrac {
+	
+	public static void main(String[] args) {
+		
+		int i = 1;
+		
+		while (i <= 10) {
+			
+			System.out.println(i);
+			i++;
+		}
+		
+		System.out.println("==================");
+		
+		for (int n = 21; n < 31; n++ ) {
+			
+			System.out.println(n);
+		}
+		
+		
+	}
+
+}
